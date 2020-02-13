09ea901a2ced426d914c28039b9b383dacb2a338
nicooffee <nicolas.honorato@usach.cl>
2020-02-13 20:32:11 -0300

    detalles

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ use termion::{cursor, clear};
 fn main() {
     let mut screen = AlternateScreen::from(stdout());
     let (max_x,max_y):(u16,u16) = termion::terminal_size().unwrap();
-    let mut part = participant::Participant::new(None,None,None,1,1,3,max_x as i32,max_y as i32);
+    let mut part = participant::Participant::new(Some(5),None,None,1,1,3,max_x as i32,max_y as i32);
     write!(screen,"{}{}",clear::All,cursor::Hide).unwrap();
     loop{
         let particle = part.get_particle();
         write!(screen,"{}{}{}",
             cursor::Goto(1,1),
             clear::CurrentLine,
-            format!("Pos particle x:{} y:{} dir: {} {}",
+            format!("max: {} {} Pos particle x:{} y:{} dir: {} {}",max_x,max_y,
                 particle.get_pos_x(),
                 particle.get_pos_y(),
                 particle.get_sym(true),
