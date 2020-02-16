233d394f603c92ae757cc920f623711917867630
= <=>
2020-02-16 10:44:18 -0300

    color added

--- particle/target/rls/debug/deps/participant_lib-917f69a205e275bf.d
+++ particle/target/rls/debug/deps/participant_lib-917f69a205e275bf.d
@@ -1,6 +1,6 @@
-/mnt/c/Users/user/Desktop/rusty/par_proj/particle/target/rls/debug/deps/participant_lib-917f69a205e275bf.rmeta: src/lib.rs src/participant/particle.rs src/participant/particle/direction.rs src/participant/particle/position.rs src/participant/limit_box.rs src/participant/limit_box/limit.rs
+/mnt/c/Users/Nico/Desktop/Rust/particle_project/Particle-Collider-Rust/particle/target/rls/debug/deps/participant_lib-917f69a205e275bf.rmeta: src/lib.rs src/participant/particle.rs src/participant/particle/direction.rs src/participant/particle/position.rs src/participant/limit_box.rs src/participant/limit_box/limit.rs
 
-/mnt/c/Users/user/Desktop/rusty/par_proj/particle/target/rls/debug/deps/participant_lib-917f69a205e275bf.d: src/lib.rs src/participant/particle.rs src/participant/particle/direction.rs src/participant/particle/position.rs src/participant/limit_box.rs src/participant/limit_box/limit.rs
+/mnt/c/Users/Nico/Desktop/Rust/particle_project/Particle-Collider-Rust/particle/target/rls/debug/deps/participant_lib-917f69a205e275bf.d: src/lib.rs src/participant/particle.rs src/participant/particle/direction.rs src/participant/particle/position.rs src/participant/limit_box.rs src/participant/limit_box/limit.rs
 
 src/lib.rs:
 src/participant/particle.rs:
