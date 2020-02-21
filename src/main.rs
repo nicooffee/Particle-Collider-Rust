e0de4caab43049d7744dd0e4ae1d073d01cc6dc2
nicooffee <nicolas.honorato@usach.cl>
2020-02-21 16:07:34 -0300

    starting w thread

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,15 @@ use participant_lib::source_list::source::limit_box::LimitBox;
 use participant_lib::source_list::source::particle::position::Position;
 use termion::screen::AlternateScreen;
 use std::{thread, time};
+use std::sync::{Arc, Mutex};
 use std::io::{Read, Write, stdout};
 use termion::{cursor, clear};
 use termion::raw::IntoRawMode;
 use termion::async_stdin;
 use termion_ext::AdvWrite;
+
+const CANT_SOURCE: u32      = 3;
+const CANT_PARTICLE: u32    = 2;
 
 
 fn main() {
@@ -17,43 +21,8 @@ fn main() {
     write!(s_out,"{}{}",clear::All,cursor::Hide).unwrap();
     s_out.w_box(1,2,max_x,max_y,None,None);
     s_out.flush().unwrap();
-    let mut source_list = SourceList::new(3,1,LimitBox::new(2,3,max_x as i32 -1,max_y as i32 -1));
-    let mut it = 1;
-    let mut c_col = 0;
-    loop{
-        let b = s_in.next();
-        if let Some(Ok(b'q')) = b {
-            break;
-        }
-        {
-            s_out.w_go_str(1,1,
-                format!("C_ACT: {}\tC_NACT: {}\tIT: {}\tIT%COL: {:.4}",
-                    source_list.get_len_active(),
-                    source_list.get_len_nactive(),
-                    it,
-                    (c_col as f32)/(it as f32)
-                )
-            );
-        }
-        for i in 0..source_list.get_len_active(){
-            let opt = source_list.move_particle(i);
-            let mut opt_col = None::<Position>;
-            if let Some((coll,pos)) = opt {
-                let p = coll.get_position();
-                s_out.w_go_str(p.get_pos_x()as u16,p.get_pos_y()as u16,String::from(" "));
-                opt_col = Some(pos);
-                c_col = c_col + 1;
-            }
-            if let Some(src) = source_list.get_source_act(i){
-                src.particle_print(&mut s_out,true);
-                if let Some(pos_col) = opt_col{
-                    s_out.w_go_str(pos_col.get_pos_x()as u16,pos_col.get_pos_y()as u16,String::from("💥"));
-                }
-                it = it + 1;
-            }
-            
-        }
-        s_out.flush().unwrap();
-        thread::sleep(time::Duration::from_millis(10));
-    }
+    let mut src_list = SourceList::new(CANT_SOURCE,CANT_PARTICLE,LimitBox::new(2,3,max_x as i32 -1,max_y as i32 -1));
+    let src_list_share = Arc::new(Mutex::new(src_list));
+
+    
 }
