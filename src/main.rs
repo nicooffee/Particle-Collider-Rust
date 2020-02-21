a4e9b6d61fcb2a4f1220d2d4f306317b29e8f7f4
nicooffee <nicolas.honorato@usach.cl>
2020-02-21 16:54:14 -0300

    share clone s_in

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use participant_lib::source_list::SourceList;
 use participant_lib::source_list::source::limit_box::LimitBox;
-use participant_lib::source_list::source::particle::position::Position;
 use termion::screen::AlternateScreen;
 use std::{thread, time};
 use std::sync::{Arc, Mutex};
@@ -10,7 +9,7 @@ use termion::raw::IntoRawMode;
 use termion::async_stdin;
 use termion_ext::AdvWrite;
 
-const CANT_SOURCE: usize      = 3;
+const CANT_SOURCE: usize      = 5;
 const CANT_PARTICLE: u32    = 2;
 
 
@@ -22,16 +21,21 @@ fn main() {
     s_out.w_box(1,2,max_x,max_y,None,None);
     s_out.flush().unwrap();
     let src_list = SourceList::new(CANT_SOURCE,CANT_PARTICLE,LimitBox::new(2,3,max_x as i32 -1,max_y as i32 -1));
-    let src_list_share = Arc::new(Mutex::new(src_list));
-    let s_out_share = Arc::new(Mutex::new(s_out));
+    
+    let share_src_list  = Arc::new(Mutex::new(src_list));
+    let share_s_out     = Arc::new(Mutex::new(s_out));
+    let share_s_in      = Arc::new(Mutex::new(s_in));
+
     let mut threads = vec![];
     for x in 0..CANT_SOURCE {
         threads.push(
             thread::spawn({
-                    let clone_src_list = Arc::clone(&src_list_share);
-                    let clone_s_out = Arc::clone(&s_out_share);
+                    let clone_src_list  = Arc::clone(&share_src_list);
+                    let clone_s_out     = Arc::clone(&share_s_out);
+                    let clone_s_in      = Arc::clone(&share_s_in);
                     move || { 
                         loop {
+                            thread::sleep(time::Duration::from_millis(20));
                             let mut src_l = clone_src_list.lock().unwrap();
                             let mut s_out = clone_s_out.lock().unwrap();
                             match src_l.get_source_act(x){
@@ -42,7 +46,6 @@ fn main() {
                                     s_out.flush().unwrap();
                                 }
                             }
-                            thread::sleep(time::Duration::from_millis(200));
                         }
                     }
                 }
